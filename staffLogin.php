b55eae3ca487ebb35b0b6e32fcd6f787521c7ead
SaishGhadi <108211307+SaishGhadi@users.noreply.github.com>
2023-11-15 11:43:35 +0530

    Update staffLogin.php    (connected to database )

--- staffLogin.php
+++ staffLogin.php
@@ -1,3 +1,50 @@
+<?php
+// Connecting to the Db start
+$server = "localhost";
+$username = "root";
+$password = "";
+$dbname = "hms";
+
+$con = mysqli_connect($server, $username, $password, $dbname);
+
+if (!$con) {
+    die("Connection to this database failed due to" . mysqli_connect_error());
+}
+
+$message = ""; // Variable to store the login result message
+
+if (isset($_POST['login'])) {
+    $staff_id = $_POST['staff_id'];
+    $password = $_POST['password'];
+
+    // Secure the input values to prevent SQL injection
+    $staff_id = mysqli_real_escape_string($con, $staff_id);
+    $password = mysqli_real_escape_string($con, $password);
+
+    $sql = "SELECT * FROM staff_details WHERE staff_id='$staff_id' AND password='$password'";
+    $result = mysqli_query($con, $sql);
+
+    if ($result) {
+        $row = mysqli_fetch_assoc($result);
+        if ($row) {
+            // Login successful
+            // Redirect to the dashboard page
+            header("Location: staff_dashboard.html");
+            exit(); // Ensure that no further code is executed after the redirection
+        } else {
+            // Invalid staff_id or password
+            $message = "Invalid staff_id or password";
+        }
+    } else {
+        // Query execution failed
+        $message = "Error executing query: " . mysqli_error($con);
+    }
+
+    // Close the database connection
+    mysqli_close($con);
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +53,15 @@
     <title>Staff Login</title>
     <link rel="stylesheet" href="staffLog.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <style>
+        body{}
+    </style>
+    <script>
+        // This script shows the error message when the login fails
+        function showErrorMessage() {
+            document.getElementById('errorMessage').style.display = 'block';
+        }
+    </script>
 </head>
 <body>
     <!-- Navigation Bar -->
@@ -22,13 +78,19 @@
     
     <!-- Login Section -->
     <div class="wrapper">
-        <form action="">
+        <form action="staffLogin.php" method="post" onsubmit="showErrorMessage()">
             <h1>Staff Login</h1>
+            
+            <!-- Error Message Section -->
+            <div id="errorMessage" style="color: red; display: <?php echo empty($message) ? 'none' : 'block'; ?>;">
+                <?php echo $message; ?>
+            </div>
+
             <div class="input-box">
-                <input type="text" placeholder="User ID" required>
+                <input type="text" name="staff_id" placeholder="Staff ID" required>
             </div>
             <div class="input-box">
-                <input type="password" placeholder="Password" required>
+                <input type="password" name="password" placeholder="Password" required>
             </div>
 
             <!-- Forgot Password Section -->
@@ -37,7 +99,7 @@
             </div>
             
             <!-- Login Button -->
-            <button type="submit" class="btn">Submit</button>
+            <button type="submit" class="btn" name="login">Submit</button>
         </form>
     </div>
 </body>
