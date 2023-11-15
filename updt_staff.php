57e7bcd0967dd8a4c1857b833b13d5730a6e57d2
SaishGhadi <108211307+SaishGhadi@users.noreply.github.com>
2023-11-15 21:43:36 +0530

    Update updt_staff.php (connected to database (fully working))

--- updt_staff.php
+++ updt_staff.php
@@ -1,3 +1,62 @@
+
+<?php
+$server = "localhost";
+$username = "root";
+$password = "";
+$dbname = "hms";
+
+$con = mysqli_connect($server, $username, $password, $dbname);
+
+if (!$con) {
+    die("Connection to this database failed due to" . mysqli_connect_error());
+}
+
+$message = ""; // Variable to store the update result message
+
+if (isset($_POST['staff_id'])) {
+    $staff_id = $_POST['staff_id'];
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $gender = $_POST['gender'];
+    $designation = $_POST['designation'];
+    $mobile_no = $_POST['mobile_no'];
+    $dob = $_POST['dob'];
+    $salary = $_POST['salary'];
+
+    // Secure the input values to prevent SQL injection
+    $staff_id = mysqli_real_escape_string($con, $staff_id);
+    $first_name = mysqli_real_escape_string($con, $first_name);
+    $last_name = mysqli_real_escape_string($con, $last_name);
+    $gender = mysqli_real_escape_string($con, $gender);
+    $designation = mysqli_real_escape_string($con, $designation);
+    $mobile_no = mysqli_real_escape_string($con, $mobile_no);
+    $dob = mysqli_real_escape_string($con, $dob);
+    $salary = mysqli_real_escape_string($con, $salary);
+
+    // Update staff details based on staff ID
+    $update_query = "UPDATE staff_details SET 
+                     fname='$first_name', 
+                     lname='$last_name', 
+                     gender='$gender', 
+                     designation='$designation', 
+                     mob_no='$mobile_no', 
+                     dob='$dob', 
+                     salary='$salary' 
+                     WHERE staff_id='$staff_id'";
+
+    $update_result = mysqli_query($con, $update_query);
+
+    if ($update_result) {
+        $message = "Staff details updated successfully!";
+    } else {
+        $message = "Error updating staff details: " . mysqli_error($con);
+    }
+}
+
+mysqli_close($con);
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +65,13 @@
     <title>Patient Details</title>
     <link rel="stylesheet" href="updt_staff.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <script>  /// FOR POPUP
+        <?php
+            if (!empty($message)) {
+                echo "window.onload = function() { alert('$message'); }";
+            }
+        ?>
+    </script>
 </head>
 <body>
 
@@ -18,17 +84,23 @@
     </ul>
 
     <div class="wrapper">
-        <form action="">
+        <form action="updt_staff.php" method="post">
             <h1>Update Staff Details</h1>
+
+            <h3>Enter the ID to be Updated</h3>
             <div class="input-box">
-                <input type="text" placeholder="First Name" required>
+                <input type="text" name="staff_id" placeholder="Staff-ID" required>
+            </div>
+            
+            <br>
+
+            <div class="input-box">
+                <input type="text" name="first_name" placeholder="First Name" required>
             </div>
             <div class="input-box">
-                <input type="text" placeholder="Last Name" required>
+                <input type="text" name="last_name" placeholder="Last Name" required>
             </div>
-            <div class="input-box">
-                <input type="text" placeholder="Staff-ID" required>
-            </div>
+
 
             <div class="input-box">
                 <span class="gender-label">Gender:</span>
@@ -42,19 +114,23 @@
             <div class="input-box">
                 <label for="designation">Choose an option:</label>
                 <select id="designation" name="designation" required>
-                    <option value="option1">Doctor</option>
-                    <option value="option2">Nurse</option>
-                    <option value="option3">Ward Boy</option>
+                    <option value="Doctor">Doctor</option>
+                    <option value="Nurse">Nurse</option>
+                    <option value="Ward Boy">Ward Boy</option>
                 </select>
             </div>
 
             <div class="input-box">
-                <input type="tel" placeholder="Mobile No." pattern="\d{10}" required>
+                <input type="tel" name="mobile_no" placeholder="Mobile No." pattern="\d{10}" required>
             </div>
             
             <div class="input-box">
                 <span class="gender-label">DOB: </span>
-                <input type="date" placeholder="Date of Birth" required>
+                <input type="date" name="dob" placeholder="Date of Birth" required>
+            </div>
+
+            <div class="input-box">
+                <input type="number" name="salary" placeholder="Salary" required>
             </div>
 
             <button type="submit" class="btn">Submit</button>
