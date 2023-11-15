639d4b77cacbde21afedd22c89b723c1ab467c21
MagnanimousDown <131783859+MagnanimousDown@users.noreply.github.com>
2023-11-15 14:22:58 +0530

    Update patient_details.php
    Added Js, connection to the table named patient_details

--- patient_details.php
+++ patient_details.php
@@ -1,3 +1,42 @@
+<?php
+    // Connecting to the Db start
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "hms";
+
+    $con = mysqli_connect($server, $username, $password, $dbname);
+
+    if (!$con) {
+        die("Connection to this database failed due to" . mysqli_connect_error());
+    }
+
+    $message = ""; // Variable to store the insertion result message
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $patient_id = $_POST['patient_id'];
+        $pfname = $_POST['pfname'];
+        $plname = $_POST['plname'];
+        $gender = $_POST['gender'];
+        $dob = $_POST['dob'];
+        $mob_no = $_POST['mob_no'];
+
+        $sql = "INSERT INTO `patient_details` (`patient_id`, `pfname`, `plname`, `gender`, `dob`, `mob_no`, `admission_dt`) 
+                VALUES ('$patient_id', '$pfname', '$plname', '$gender', '$dob', '$mob_no', NOW());";
+
+        $result = mysqli_query($con, $sql);
+
+        if ($result) {
+            $message = "Patient details added successfully!";
+        } else {
+            $message = "Error: " . mysqli_error($con);
+        }
+    }
+
+    // Close the database connection
+    mysqli_close($con);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,30 +45,36 @@
     <title>Patient Details</title>
     <link rel="stylesheet" href="PD-style.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+    <script>
+        <?php
+            if (!empty($message)) {
+                echo "window.onload = function() { alert('$message'); }";
+            }
+        ?>
+    </script>
 </head>
 <body>
     
     <!-- Navigation Bar -->
     <ul class="navbar">
         <li><a href="staff_dashboard.html">Home</a></li>
-
-
         <ul class="SignLog">
-        <li><a href="logins.php">LogOut</a></li>
+            <li><a href="logins.php">LogOut</a></li>
         </ul>
     </ul>
 
     <div class="wrapper">
-        <form action="">
+        <form action="patient_details.php" method="post">
             <h1>Patient Details</h1>
             <div class="input-box">
-                <input type="text" placeholder="First Name" required>
+                <input type="text" name="pfname" placeholder="First Name" required>
             </div>
             <div class="input-box">
-                <input type="text" placeholder="Last Name" required>
+                <input type="text" name="plname" placeholder="Last Name" required>
             </div>
             <div class="input-box">
-                <input type="text" placeholder="Patient-ID" required>
+                <input type="text" name="patient_id" placeholder="Patient-ID" required>
             </div>
 
             <div class="input-box">
@@ -43,12 +88,12 @@
             </div>
 
             <div class="input-box">
-                <input type="tel" placeholder="Mobile No." pattern="\d{10}" required>
+                <input type="tel" name="mob_no" placeholder="Mobile No." pattern="\d{10}" required>
             </div>
             
             <div class="input-box">
                 <span class="gender-label">DOB: </span>
-                <input type="date" placeholder="Date of Birth" required>
+                <input type="date" name="dob" placeholder="Date of Birth" required>
             </div>
 
             <button type="submit" class="btn">Submit</button>
