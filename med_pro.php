b6777f600e364867a3bdb05d5cd0e7226f747e2a
MagnanimousDown <131783859+MagnanimousDown@users.noreply.github.com>
2023-11-15 16:49:33 +0530

    Update med_pro.php (Updated Medical_procedure)
    Completed Medical_procedure successfully.

--- med_pro.php
+++ med_pro.php
@@ -1,3 +1,41 @@
+<?php
+    // Connecting to the Db start
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "hms";
+
+    $con = mysqli_connect($server, $username, $password, $dbname);
+
+    if (!$con) {
+        die("Connection to this database failed due to" . mysqli_connect_error());
+    }
+
+    $message = ""; // Variable to store the insertion result message
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $procedure_id = $_POST['procedure_id'];
+        $staff_id = $_POST['staff_id'];
+        $patient_id = $_POST['patient_id'];
+        $procedure_name = $_POST['procedure_name'];
+        $procedure_desc = $_POST['procedure_desc'];
+
+        $sql = "INSERT INTO `medical_procedure` (`procedure_id`, `staff_id`, `patient_id`, `procedure_name`, `procedure_date`, `procedure_desc`) VALUES ('$procedure_id', '$staff_id', '$patient_id', '$procedure_name', NOW(), '$procedure_desc');";
+
+        $result = mysqli_query($con, $sql);
+
+        if ($result) {
+            $message = "Medical Procedure Generated successfully!";
+        } else {
+            $message = "Error: " . mysqli_error($con);
+        }
+    }
+
+    // Close the database connection
+    mysqli_close($con);
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,6 +43,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Medical Procedure</title>
     <link rel="stylesheet" href="med_pro.css">
+
+    <script>
+        <?php
+            if (!empty($message)) {
+                echo "window.onload = function() { alert('$message'); }";
+            }
+        ?>
+    </script>
+
 </head>
 <body>
     <!-- Navigation Bar -->
@@ -19,26 +66,26 @@
 
     
     <div class="wrapper">
-        <form action="">
+        <form action="med_pro.php" method="post">
             <h1>Medical Procedure</h1>
             <div class="input-box">
-                <input type="text" placeholder="procedure-ID" required>
+                <input type="text" name="procedure_id" placeholder="procedure-ID" required>
             </div>
             <div class="input-box">
-                <input type="text" placeholder="Staff-ID" required>
+                <input type="text" name="staff_id" placeholder="Staff-ID" required>
             </div>
             <div class="input-box">
-                <input type="text" placeholder="Patient-ID" required>
+                <input type="text" name="patient_id" placeholder="Patient-ID" required>
             </div>
             <div class="input-box">
-                <input type="text" placeholder="Procedure Name" required>
+                <input type="text" name="procedure_name" placeholder="Procedure Name" required>
             </div>
             <div class="input-box">
                 <span class="gender-label">Procedure Date: </span>
-                <input type="date" placeholder="Procedure Date" required>
+                <input type="date" name="procedure_date" placeholder="Procedure Date" required>
             </div>
             <div class="input-box">
-                <input type="text" placeholder="Procedure Description" required>
+                <input type="text" name="procedure_desc" placeholder="Procedure Description" required>
             </div>
             
             <button type="submit" class="btn">Submit</button>
