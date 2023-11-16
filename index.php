25a8ffbb6f78cb63d62c7306599ba677cf3d3d44
SaishGhadi <108211307+SaishGhadi@users.noreply.github.com>
2023-11-16 22:49:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Patient Records and Treatment</title>
+    <title>H M S</title>
     <link rel="stylesheet" href="style.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
+
 <body>
     <!-- Navigation Bar -->
     <ul class="navbar">
@@ -15,58 +17,57 @@
         <li><a href="#">Services</a></li>
         <li><a href="#">Contact</a></li>
         <ul class="SignLog">
-        <li><a href="logins.php">Login</a></li>
+            <li><a href="logins.php">Login</a></li>
         </ul>
     </ul>
-    
+
     <!-- Login Section -->
     <div class="wrappert">
         <form action="" method="post">
             <h1>View Patient Report & Medical Treatment Procedure</h1>
-            
+
             <div class="input-box">
                 <input type="text" name="patient_id" placeholder="Patient ID" required>
             </div>
-            
+
             <div class="button-container">
                 <button type="submit" name="patient_records" class="btn">Patient Records</button>
-                <button type="submit" name="treatment_procedure" class="btn">Patient Treatment Procedure</button>
             </div>
         </form>
     </div>
 
     <div class="wrapperb">
         <?php
+
+        $server = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "hms";
+
+        // Create connection
+        $conn = new mysqli($server, $username, $password, $dbname);
+
+        // Check the connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
         // Check if the form has been submitted
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $server = "localhost";
-            $username = "root";
-            $password = "";
-            $dbname = "hms";       
-            
-            // Create connection
-            $conn = new mysqli($server, $username, $password, $dbname);
-            
-            // Check the connection
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            }
-
-            $patient_id = $_POST["patient_id"];
 
             if (isset($_POST["patient_records"])) {
                 // Fetch and display patient records only if patient ID is provided
-                if (!empty($patient_id)) {
-                    $result = $conn->query("SELECT * FROM patient_report WHERE patient_id = '$patient_id'");
-                    displayResults($result);
-                } else {
-                    echo "Please enter a valid Patient ID.";
-                }
-            } elseif (isset($_POST["treatment_procedure"])) {
-                // Fetch and display treatment procedures only if patient ID is provided
-                if (!empty($patient_id)) {
-                    $result = $conn->query("SELECT * FROM medical_procedure WHERE patient_id = '$patient_id'");
-                    displayResults($result);
+                if (isset($_POST["patient_id"])) {
+                    $patient_id = $_POST["patient_id"];
+                    if (!empty($patient_id)) {
+                        $sql = "SELECT sd.staff_id, sd.fname, sd.lname, sd.gender, sd.designation, sd.mob_no, sd.dob, sd.salary FROM staff_details sd 
+                                JOIN patient_report pr ON sd.staff_id = pr.staff_id
+                                WHERE pr.patient_id = '$patient_id'";
+                        $result = $conn->query($sql);
+                        displayResults($result);
+                    } else {
+                        echo "Please enter a valid Patient ID.";
+                    }
                 } else {
                     echo "Please enter a valid Patient ID.";
                 }
@@ -77,19 +78,38 @@
         }
 
         // Function to display query results in a table
-        function displayResults($result) {
-            echo "<table border='1'>";
-            while ($row = $result->fetch_assoc()) {
-                echo "<tr>";
-                foreach ($row as $value) {
-                    echo "<td>$value</td>";
+        function displayResults($result)
+        {
+            if ($result->num_rows > 0) {
+                echo "<table class='coach-table'>";
+                echo "<tr><th>Staff ID</th><th>First Name</th><th>Last Name</th><th>Gender</th><th>Designation</th><th>Mobile No</th><th>Date of Birth</th><th>Salary</th></tr>";
+                while ($row = $result->fetch_assoc()) {
+                    echo "<tr>";
+                    echo "<td>" . $row["staff_id"] . "</a></td>";
+                    echo "<td>" . $row["fname"] . "</td>";
+                    echo "<td>" . $row["lname"] . "</td>";
+                    echo "<td>" . $row["gender"] . "</td>";
+                    echo "<td>" . $row["designation"] . "</td>";
+                    echo "<td>" . $row["mob_no"] . "</td>";
+                    echo "<td>" . $row["dob"] . "</td>";
+                    echo "<td>" . $row["salary"] . "</td>";
+                    echo "</tr>";
                 }
-                echo "</tr>";
+                echo "</table>";
+            } else {
+                echo "<p>No Staff Member found</p>";
             }
-            echo "</table>";
         }
         ?>
     </div>
 
+    <!-- Refresh button outside the form -->
+    <div class="button-container">
+        <form action="" method="post">
+            <button type="submit" name="refresh" class="btn">Refresh</button>
+        </form>
+    </div>
+
 </body>
+
 </html>
