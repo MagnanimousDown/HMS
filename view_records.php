aea4ba3ac407a487e46e7b3f3a33110c40639d37
SaishGhadi <108211307+SaishGhadi@users.noreply.github.com>
2023-11-22 11:55:26 +0530

    Update view_records.php    full

--- view_records.php
+++ view_records.php
@@ -1,72 +1,74 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Patient Records and Treatment</title>
+    <title>View Records</title>
     <link rel="stylesheet" href="view_records.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
+
 <body>
     <!-- Navigation Bar -->
     <ul class="navbar">
-        <li><a href="staff_dashboard.php">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Services</a></li>
-        <li><a href="#">Contact</a></li>
+        <li><a href="staff_dashboard.html">Home</a></li>
+
         <ul class="SignLog">
-        <li><a href="logins.php">LogOut</a></li>
+            <li><a href="logins.php">LogOut</a></li>
         </ul>
     </ul>
-    
+
     <!-- Login Section -->
     <div class="wrappert">
         <form action="" method="post">
             <h1>View Patient Report & Medical Treatment Procedure</h1>
-            
+
             <div class="input-box">
                 <input type="text" name="patient_id" placeholder="Patient ID" required>
             </div>
-            
+
             <div class="button-container">
                 <button type="submit" name="patient_records" class="btn">Patient Records</button>
-                <button type="submit" name="treatment_procedure" class="btn">Patient Treatment Procedure</button>
             </div>
         </form>
     </div>
 
     <div class="wrapperb">
         <?php
+
+        $server = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "hms";
+
+        // Create connection
+        $conn = new mysqli($server, $username, $password, $dbname);
+
+        // Check the connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
         // Check if the form has been submitted
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $server = "localhost";
-            $username = "root";
-            $password = "";
-            $dbname = "hms";       
-            
-            // Create connection
-            $conn = new mysqli($server, $username, $password, $dbname);
-            
-            // Check the connection
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            }
-
-            $patient_id = $_POST["patient_id"];
 
             if (isset($_POST["patient_records"])) {
                 // Fetch and display patient records only if patient ID is provided
-                if (!empty($patient_id)) {
-                    $result = $conn->query("SELECT * FROM patient_report WHERE patient_id = '$patient_id'");
-                    displayResults($result);
-                } else {
-                    echo "Please enter a valid Patient ID.";
-                }
-            } elseif (isset($_POST["treatment_procedure"])) {
-                // Fetch and display treatment procedures only if patient ID is provided
-                if (!empty($patient_id)) {
-                    $result = $conn->query("SELECT * FROM medical_procedure WHERE patient_id = '$patient_id'");
-                    displayResults($result);
+                if (isset($_POST["patient_id"])) {
+                    $patient_id = $_POST["patient_id"];
+                    if (!empty($patient_id)) {
+                        $sql = "SELECT pd.patient_id, pd.pfname, pd.plname, pr.disease, pr.diagnosis, mp.procedure_name, mp.procedure_desc
+                                FROM patient_details pd
+                                JOIN patient_report pr ON pd.patient_id = pr.patient_id
+                                JOIN medical_procedure mp ON pr.patient_id = mp.patient_id
+                                WHERE pd.patient_id = '$patient_id'";
+                        
+                        $result = $conn->query($sql);
+                        displayResults($result);
+                    } else {
+                        echo "Please enter a valid Patient ID.";
+                    }
                 } else {
                     echo "Please enter a valid Patient ID.";
                 }
@@ -77,19 +79,37 @@
         }
 
         // Function to display query results in a table
-        function displayResults($result) {
-            echo "<table border='1'>";
-            while ($row = $result->fetch_assoc()) {
-                echo "<tr>";
-                foreach ($row as $value) {
-                    echo "<td>$value</td>";
+        function displayResults($result)
+        {
+            if ($result->num_rows > 0) {
+                echo "<table class='coach-table'>";
+                echo "<tr><th>Patient ID</th><th>First Name</th><th>Last Name</th><th>Disease</th><th>Diagnosis</th><th>Procedure Name</th><th>Procedure Description</th></tr>";
+                while ($row = $result->fetch_assoc()) {
+                    echo "<tr>";
+                    echo "<td>" . $row["patient_id"] . "</td>";
+                    echo "<td>" . $row["pfname"] . "</td>";
+                    echo "<td>" . $row["plname"] . "</td>";
+                    echo "<td>" . $row["disease"] . "</td>";
+                    echo "<td>" . $row["diagnosis"] . "</td>";
+                    echo "<td>" . $row["procedure_name"] . "</td>";
+                    echo "<td>" . $row["procedure_desc"] . "</td>";
+                    echo "</tr>";
                 }
-                echo "</tr>";
+                echo "</table>";
+            } else {
+                echo "<p>No Records found</p>";
             }
-            echo "</table>";
         }
         ?>
     </div>
 
+    <!-- Refresh button outside the form -->
+    <div class="button-container">
+        <form action="" method="post">
+            <button type="submit" name="refresh" class="btn">Refresh</button>
+        </form>
+    </div>
+
 </body>
+
 </html>
